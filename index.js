3f136a666342139c3bd95d1bdebad6da26a5c944
Saad Khalid <saadkhalid913@gmail.com>
2021-06-08 22:05:33 -0400

    added mongose link as config variable

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const cors = require("cors")
-mongoose.connect("mongodb://localhost:27017/TaskPlanner")
+const config = require("config");
+
+const mongoose_link = (config.get("mongoose_link"))
+if (!mongoose_link) throw new Error("No mongoose cluster specified")
+console.log(mongoose_link)
+
+mongoose.connect(mongoose_link)
 
 const TaskRouter = require("./api/routes/Tasks")
 const CategoryRouter = require("./api/routes/Categories")
 
-const subtask = require("./api/Schemas/Schemas").SubTaskModel;
-const { eventNames } = require("./api/Schemas/CategoryModel");
-
-
 const app = express()
 
 app.use(express.json())
-app.use(cors({origin: "*"}))
+app.use(cors({ origin: "*" }))
 app.use("/api/tasks", TaskRouter) // add validation to all routes 
 app.use("/api/categories", CategoryRouter) // add validation to schemas 
 
-            
+
 
 const PORT = 3000 || process.env.PORT
 
